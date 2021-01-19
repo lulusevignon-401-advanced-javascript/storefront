c9f2f146f0be0141b20070859f0a80ed9f68ed1c
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-01-19 12:59:46 -0800

    mergin into master

--- src/store/categories.js
+++ src/store/categories.js
@@ -1,25 +1,10 @@
 // //define initial state
-// import axios from 'axios';
+import axios from 'axios';
 
-// const initialState = {
-//   categories: [
-//     { name: 'electronics', displayName: 'Electronics' },
-//     { name: 'food', displayName: 'Food' },
-//   ],
-//   products: [
-//   //   { name: 'Monitor', category: 'electronics', price: 699.00, inStock: 5 },
-//   //   { name: 'Mouse', category: 'electronics', price: 99.00, inStock: 15 },
-//   //   { name: '1 TB USB', category: 'electronics', price: 75.00, inStock: 23 },
-//   //   { name: 'Apples', category: 'food', price: .99, inStock: 500 },
-//   //   { name: 'carrots', category: 'food', price: 1.99, inStock: 120 },
-//   //   { name: 'Calzones', category: 'food', price: 4.39, inStock: 90 },
-//   ],
-//   activeCategory: 'electronics'
-
-// }
+// const initialState = {categories: [], activeCategory: ''};
 
 // //define reducer
-// export default (state = initialState, action) =>{
+// export default function categoryReducer (state = initialState, action){
 //   let {type, payload} = action;
 
 //   switch (type) {
@@ -46,7 +31,14 @@
 //   }
 // }
 
-import superagent from 'superagent';
+
+
+
+
+
+
+
+// import superagent from 'superagent';
 
 import { createSlice } from '@reduxjs/toolkit'
 
@@ -67,8 +59,8 @@ const categorySlice = createSlice({
 })
 
 export const getCategories = () => async dispatch => {
-  let response = await superagent.get('https://api-js401.herokuapp.com/api/v1/categories');
-  dispatch(setCategoryList(response.body.results));
+  let response = await axios.get('https://api-js401.herokuapp.com/api/v1/categories');
+  dispatch(setCategoryList(response.data.results));
 };
 
 // Not publishing the internal action, only the thunk'd one
